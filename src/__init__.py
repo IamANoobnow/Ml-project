e78cac270e1724fe36c874c6d0e5fd7eb53d6592
spacemanneverborn <spacemanneverborn@gmail.com>
2024-08-14 02:36:19 +0530

    after setup.py

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+#whenever find packages is runnning it will go and see in how many folders __init__ file is there
+#It will directly consider the source as the package itself and then try to build this
